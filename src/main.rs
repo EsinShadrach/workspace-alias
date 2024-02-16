7b966307844a8b92d4b3b5166b5061489a6d5e73
EsinShadrach <esinshadrachrafe@gmail.com>
2024-02-16 14:55:55 +0100

    Added colored_print_alias

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use colorful::Color;
+use colorful::Colorful;
+
 fn main() {
-    println!("Hello, world!");
+    let alias_name = String::from("run");
+    let command_name = String::from("bun dev");
+
+    print_alias(alias_name, command_name);
 }
+
+fn print_alias(alias: String, command: String) {
+    let colored_alias = format!("alias {alias}")
+        .color(Color::Cyan)
+        .bold()
+        .underlined();
+    let colored_command = format!("command {command}")
+        .color(Color::Yellow)
+        .bold()
+        .underlined();
+
+    let display_text = format!("{colored_alias} created for {colored_command}");
+
+    println!("{display_text}");
+}
+
+/*
+ * We need to know the workspace we're on
+ * Create alias for it and output `alias x created for command x`
+ * */
