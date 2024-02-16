8760668c1f270ed109a3989aa6315c00e25352ff
EsinShadrach <esinshadrachrafe@gmail.com>
2024-02-16 19:55:30 +0100

    Added comments...

--- src/utils/convert_to_camel_case.rs
+++ src/utils/convert_to_camel_case.rs
@@ -2,23 +2,26 @@ pub fn to_camel_case(text: String) -> String {
     let mut camel_cased = String::new();
     // "My Name" => ["My", "Name"]
     let splitted: Vec<&str> = text.split_whitespace().collect();
-    // We want to loop through all of these, and make Each first Letter a title case
 
     for (index, word) in splitted.iter().enumerate() {
         match index {
             0 => {
+                // Convert first word to lower_case and just push it
                 camel_cased.push_str(&word.to_lowercase());
             }
             _ => {
-                let first_letter = word
-                    .chars()
-                    .next()
-                    .expect("Failed to get First Letter")
-                    .to_uppercase();
+                let first_letter = word.chars().next();
 
-                let mut pushed_to_camel_case = first_letter.to_string();
-                pushed_to_camel_case.push_str(&word);
-                camel_cased.push_str(&pushed_to_camel_case);
+                match first_letter {
+                    Some(x) => {
+                        let mut pushed_to_camel_case = first_letter.to_string();
+                        pushed_to_camel_case.push_str(&word);
+                        camel_cased.push_str(&pushed_to_camel_case);
+                    }
+                    None => (),
+                }
+                // .expect("Failed to get First Letter")
+                // .to_uppercase();
             }
         }
     }
