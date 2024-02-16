724ef67fc0067edd1a0eec8f003bd622d7a7f3ab
EsinShadrach <esinshadrachrafe@gmail.com>
2024-02-16 23:04:37 +0100

    ]Completed Write to SHELL config function

--- src/core/create_alias.rs
+++ src/core/create_alias.rs
@@ -1,4 +1,4 @@
-use std::{env, process::Command};
+use std::{env, fs::OpenOptions, io::Write, process::Command};
 
 use crate::{
     utils::{convert_to_camel_case::to_camel_case, print_alias::print_alias},
@@ -12,7 +12,7 @@ pub fn create_alias(alias: &Alias) {
 
     match execute_alias_creation {
         Ok(_) => {
-            write_to_config();
+            write_to_config(alias);
             print_alias(alias.clone());
         }
         Err(err) => {
@@ -22,12 +22,36 @@ pub fn create_alias(alias: &Alias) {
     }
 }
 
-fn write_to_config() {
-    let config = match env::var("SHELL") {
-        Ok(s) if s.contains("zsh") => "~/.zshrc",
-        Ok(s) if s.contains("bash") => "~/.bashrc",
+fn write_to_config(alias: &Alias) {
+    let command = &alias.command;
+    let alias = &alias.alias;
+
+    let config_file_path = match env::var("SHELL") {
+        Ok(s) if s.contains("zsh") => ".zshrc",
+        Ok(s) if s.contains("bash") => ".bashrc",
         _ => {
-            panic!("Un-Supported SHELL type")
+            panic!("Unsupported SHELL type")
         }
     };
+
+    let alias_command = format!("alias {}={}\n", alias, command);
+
+    match env::var("HOME") {
+        Ok(home_path) => {
+            let total_path = format!("{home_path}/{config_file_path}");
+            println!("{}", total_path);
+            match OpenOptions::new().append(true).open(total_path) {
+                Ok(mut file) => match file.write_all(alias_command.as_bytes()) {
+                    Err(e) => {
+                        eprintln!("Failed to write alias to configuration file: {}", e);
+                    }
+                    _ => (),
+                },
+                Err(_) => {
+                    eprintln!("Failed to open configuration file for writing");
+                }
+            }
+        }
+        _ => println!("Failed go get HOME Environment"),
+    }
 }
