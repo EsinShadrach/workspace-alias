2f00b4bc80f7cdc7094cf0bf4b3f4bfbd73fbd17
EsinShadrach <EsinShadrach@users.noreply.github.com>
2024-05-04 11:24:09 +0000

    deploy: d2f01e32926d0dccc83e8a97d4519215748d0573

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["alias_thing",["",[["core",[],["config_operations.rs","create_alias_in_shell.rs","create_config.rs","create_lang_map.rs","determin_workspace.rs","mod.rs"]],["utils",[],["get_shell.rs","log_err_msg.rs","mod.rs","useful_utils.rs"]]],["main.rs"]]]\
+]'));
+createSrcSidebar();
